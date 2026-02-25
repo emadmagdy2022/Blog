de1a0d6de83de0efe53ce185f4972946a4a81d35
Emad Magdy <63416072+emadmagdy2022@users.noreply.github.com>
2026-02-25 14:50:45 +0200

    creating models user ,post,comment,and like

--- blogapi/models.py
+++ blogapi/models.py
@@ -3,5 +3,30 @@ from django.contrib.auth.models import AbstractUser
 
 class User (AbstractUser):
     image = models.ImageField(upload_to= 'profile_images/',blank=True, null=True)
-    
 
+class Post (models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):      
+        return f'Comment by {self.author.username} on {self.post.title}'
+
+class Like (models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        constraints = [
+            models.UniqueConstraint (fields=['user', 'post'],
+                                      name='unique_like')
+        ]
