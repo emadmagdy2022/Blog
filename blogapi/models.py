140865bc00067b46bd0c36c385eb596149bc48da
Emad Magdy <63416072+emadmagdy2022@users.noreply.github.com>
2026-02-25 14:44:56 +0200

    first commit

---
+++ blogapi/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User (AbstractUser):
+    image = models.ImageField(upload_to= 'profile_images/',blank=True, null=True)
+    
+
