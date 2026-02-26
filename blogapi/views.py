5ddb6bd3ed16c61e0e6d16531105b81b1d7905e3
Emad Magdy <63416072+emadmagdy2022@users.noreply.github.com>
2026-02-26 16:18:52 +0200

    making user viewset

--- blogapi/views.py
+++ blogapi/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
-from rest_framework import  generics
+from rest_framework import  generics,filters,permissions
 from .models import User, Post, Comment, Like
 from .serializers import UserSerializer, PostSerializer, CommentSerializer, LikeSerializer
 
 class UserViewSet(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    
-    
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [permissions.IsAdminUser()]
+        return [permissions.AllowAny()]
+    filter_backends = [filters.SearchFilter,filters.OrderingFilter]
+    search_fields = ['username', 'email']
+    ordering_fields = ['username', 'email']
