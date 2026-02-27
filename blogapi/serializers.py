a0a6ee937b19eaaff4cd0389e53744ac2fd3af15
Emad Magdy <63416072+emadmagdy2022@users.noreply.github.com>
2026-02-27 16:06:46 +0200

    altring post serializer
    Making the author serializer and comments serializer to write and read instead of read only and make id to read only

--- blogapi/serializers.py
+++ blogapi/serializers.py
@@ -15,15 +15,17 @@ class LikeSerializer (serializers.ModelSerializer):
     
 class CommentSerializer (serializers.ModelSerializer):
     author = UserSerializer(read_only=True)
+    id = serializers.IntegerField(read_only=True)
     class Meta:
         model = Comment
         fields = ['id', 'author', 'content', 'created_at']
 
 class PostSerializer (serializers.ModelSerializer):
-    author = UserSerializer(read_only=True)
-    comments = CommentSerializer(many=True, read_only=True)
+    # author = UserSerializer(read_only=True)
+    # comments = CommentSerializer(many=True, read_only=True)
     total_likes = serializers.SerializerMethodField()
     total_comments = serializers.SerializerMethodField()
+    id = serializers.IntegerField(read_only=True)
     class Meta:
         model = Post
         fields = ['id', 'title', 'content', 'author', 'created_at', 'updated_at', 'comments', 'total_likes', 'total_comments']
