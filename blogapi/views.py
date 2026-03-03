6d71532026aa2d7b8f1c63ed927d17dafca9482c
Emad Magdy <63416072+emadmagdy2022@users.noreply.github.com>
2026-03-03 16:13:53 +0200

    remove comments

--- blogapi/views.py
+++ blogapi/views.py
@@ -4,16 +4,7 @@ from .models import User, Post, Comment, Like
 from .serializers import UserSerializer, PostSerializer, CommentSerializer, LikeSerializer
 from rest_framework.decorators import action
 from rest_framework.response import Response
-# class UserViewSet(generics.ListCreateAPIView):
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
-#     def get_permissions(self):
-#         if self.request.method == 'GET':
-#             return [permissions.IsAdminUser()]
-#         return [permissions.AllowAny()]
-#     filter_backends = [filters.SearchFilter,filters.OrderingFilter]
-#     search_fields = ['username', 'email']
-#     ordering_fields = ['username', 'email']
+from django.db import transaction
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset =User.objects.all()
@@ -39,7 +30,8 @@ class PostViewSet(viewsets.ModelViewSet):
      ordering_fields = ['created_at', 'updated_at']
      def perform_create(self, serializer):
           serializer.save(author=self.request.user)
-           
+     def destroy(self, request, *args, **kwargs):
+          
      #@action(detail=True,methods=['post'],permission_classes=[permissions.IsAuthenticated])
 
 
