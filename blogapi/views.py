278dd3a2313d6a9a2745279aa87c5314054d3ed4
Emad Magdy <63416072+emadmagdy2022@users.noreply.github.com>
2026-03-03 19:31:44 +0200

    making comments and comments details methods
    list all post comment and create them
    making partial and complete update and delete to comments

--- blogapi/views.py
+++ blogapi/views.py
@@ -52,9 +52,58 @@ class PostViewSet(viewsets.ModelViewSet):
                     return super().update(request, *args, **kwargs)
                else:
                     return Response({'detail': 'You do not have permission to update this post.'}, status=403)
-     #@action(detail=True,methods=['post'],permission_classes=[permissions.IsAuthenticated])
-
-
-
+     
+     # @action(detail = True, methods=['post'], permission_classes=[permissions.IsAuthenticated], url_path='comment')
+     # def makeComment(self,request,pk):
+     #      post = self.get_object()
+     #      serializer = CommentSerializer(data=request.data)
+     #      if serializer.is_valid():
+     #           serializer.save(author=request.user, post=post)
+     #           return Response(serializer.data, status=201)
+     #      else:
+     #           return Response(serializer.errors, status=400)
+     
+     @action(detail = True,methods= ['get','post'], url_path='comments',
+             serializer_class=CommentSerializer)
+     def comments(self,request, pk=None):
+          if request.method == 'GET':
+               post = self.get_object()
+               comments = post.comments.all()
+               serializer = CommentSerializer(comments, many=True)
+               return Response(serializer.data)
+          #there is no need to make is authinticated because the permission class of the viewset is IsAuthenticatedOrReadOnly
+          elif request.method == 'POST':
+               post = self.get_object()
+               serializer = CommentSerializer(data=request.data)
+               if serializer.is_valid():
+                    serializer.save(author=request.user, post=post)
+                    return Response(serializer.data, status=201)
+               else:
+                    return Response(serializer.errors, status=400)
+     #using regix to capture the comment id in the url and then check if the user is the author of the comment before allowing them to update or delete it
+     @action(detail=True,methods = ['put','patch','delete'],url_path = 'comments/(?P<comment_id>[^/.]+)',serializer_class=CommentSerializer)
+     def comment_detail (self,request,pk=None,comment_id=None):
+          if request.method in ['PUT','PATCH']:
+               with transaction.atomic():
+                    comment = Comment.objects.get(id=comment_id,post_id=pk)
+                    if comment.author == request.user:
+                         serializer = CommentSerializer(comment,data=request.data, partial=(request.method == 'PATCH'))
+                         if serializer.is_valid():
+                              serializer.save()
+                              return Response(serializer.data)
+                         else:
+                              return Response(serializer.errors, status=400)
+                    else:
+                         return Response({'detail': 'You do not have permission to update this comment.'}, status=403)
+          elif request.method == 'DELETE':
+               with transaction.atomic():
+                    comment = Comment.objects.get(id=comment_id,post_id=pk)
+                    if comment.author == request.user:
+                         comment.delete()
+                         return Response(status=204)
+                    else:
+                         return Response({'detail': 'You do not have permission to delete this comment.'}, status=403)
+          
+          
 
 
