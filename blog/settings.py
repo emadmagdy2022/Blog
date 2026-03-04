c096ed3791d93119a6f1900cfb06d7c891124872
Emad Magdy <63416072+emadmagdy2022@users.noreply.github.com>
2026-03-04 16:59:19 +0200

    adding redis

--- blog/settings.py
+++ blog/settings.py
@@ -136,3 +136,13 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 100,
 }
 AUTH_USER_MODEL = 'blogapi.User'
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
